e41814400c88af247adc1fbcfc0708a72fb601d5
support@apple2hand.comoj <support@apple2hand.com>
2014-09-21 05:40:26 +0700

    Revert "remove pointless tabs from beginning of each line"
    This reverts commit dcad6e73a0e6efdd60dd871b47cb4812a0751620.

--- example.php
+++ example.php
@@ -1,20 +1,17 @@
 <?php
 
-/**
- * Firmware Links API for PHP Example
- * (C) 2012 Callum Jones <cj@icj.me>
- */
+	/**
+	 * Firmware Links API for PHP Example
+	 * (C) 2012 Callum Jones <cj@icj.me>
+	 */
+	require "class.firmwarelinks-api.php";
 
-require "class.firmwarelinks-api.php";
-
-try {
-
-	$fw = new FirmwareLinks();
-	$download_url = $fw->getData("url", "iPhone4,1", "5.1");
-	echo $download_url;
-
-} catch (Exception $e) {
-	echo "Caught exception: " .  $e->getMessage() . "\n";
-}
+	try {
+		$fw = new FirmwareLinks();
+		$download_url = $fw->getData("url", "iPhone4,1", "5.1");
+		echo $download_url;
+	} catch (Exception $e) {
+		echo "Caught exception: " .  $e->getMessage() . "\n";
+	}
 
 ?>
